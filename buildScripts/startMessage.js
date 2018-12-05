b6c9ee071e067e44e9d5a8c59a132ee5e0542d0f
Jones, Cody <13834439+jonesca@users.noreply.github.com>
2018-12-05 08:10:46 -0700

    Lots of changes to commit

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,6 @@
 //chalk allows color in message sent to console
-var chalk = require('chalk');
+//var chalk = require('chalk');
+//newer JS
+import chalk from 'chalk';
 
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
